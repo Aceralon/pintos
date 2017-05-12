a3ab27e86710d4eb1f792757a338ec24c061f818
aceralon <aceralon@outlook.com>
2017-05-12 13:39:52 +0800

    have a try

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -151,6 +151,7 @@ bool is_higher_priority(const struct list_elem *a, const struct list_elem *b, vo
 
 bool lock_is_higher_priority(const struct list_elem *a, const struct list_elem *b, void *aux UNUSED);
 void check_priority(struct thread *th);
+//lab4
 bool sema_is_higher_priority(const struct list_elem *a, const struct list_elem *b, void *aux UNUSED);
 
 #endif /* threads/thread.h */
